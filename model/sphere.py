bf03283842b4d0f578768aacc01c0472bf0cc6a7
wivest <120594169+wivest@users.noreply.github.com>
2025-03-24 23:17:59 +0100

    fix: check ray direction

--- model/sphere.py
+++ model/sphere.py
@@ -19,6 +19,7 @@ class Sphere:
     @ti.func
     def intersects(self, ray: Ray) -> bool:  # type: ignore
         line = ray.direction
-        tmp = self.origin - ray.origin
-        dist = ti.math.cross(line, tmp).norm() / line.norm()
-        return dist < self.radius
+        to_center = self.origin - ray.origin
+        dist = ti.math.cross(line, to_center).norm() / line.norm()
+        dot = ti.math.dot(line, to_center)
+        return dist < self.radius and dot >= 0
