f197cdaf7f28d67c4f39add468c059d971b554fe
wivest <120594169+wivest@users.noreply.github.com>
2025-03-26 23:11:18 +0100

    feat: add second sphere

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from model.sphere import Sphere
 ti.init()
 
 SIZE = (1080, 720)
-SPHERES = Sphere.field(shape=1)
+SPHERES = Sphere.field(shape=2)
 SPHERES[0] = Sphere(10, Vector((100, 0, 0)), Vector((1, 1, 0)))
+SPHERES[1] = Sphere(10, Vector((50, 0, 0)), Vector((1, 1, 0)))
 
 
 app = App("Ray Tracing", SIZE, SPHERES)
