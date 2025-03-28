2f7edb0fc367ea749070f3d9cbe741fe5fa1f8c2
wivest <120594169+wivest@users.noreply.github.com>
2025-03-28 23:41:48 +0100

    chore: edit scene

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ from model.sphere import Sphere
 ti.init()
 
 SIZE = (1080, 720)
-SPHERES = Sphere.field(shape=2)
-SPHERES[0] = Sphere(10, Vector((100, 0, 0)), Vector((1, 1, 0)))
-SPHERES[1] = Sphere(10, Vector((50, 0, 0)), Vector((1, 1, 0)))
+SPHERES = Sphere.field(shape=3)
+SPHERES[0] = Sphere(10, Vector((50, 0, 0)), Vector((1, 1, 0)))
+SPHERES[1] = Sphere(10, Vector((25, 0, 0)), Vector((1, 1, 0)))
+SPHERES[2] = Sphere(10, Vector((25, 0, 25)), Vector((1, 1, 0)))
 
 
 app = App("Ray Tracing", SIZE, SPHERES)
