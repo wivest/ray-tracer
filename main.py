dd62a1a5d092a33dffe7c97c254297ab58739eb7
wivest <120594169+wivest@users.noreply.github.com>
2025-04-07 18:09:29 +0200

    chore: add ground sphere

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ from model.material import Material
 ti.init(arch=ti.gpu)
 
 SIZE = (1080, 720)
-SPHERES = Sphere.field(shape=4)
+SPHERES = Sphere.field(shape=5)
 SPHERES[0] = Sphere(10, Vector((50, 0, 0)), Material(Vector((1.0, 0.25, 0.25)), 0.5))
 SPHERES[1] = Sphere(10, Vector((25, 0, 0)), Material(Vector((0.25, 1.0, 0.25)), 0.5))
 SPHERES[2] = Sphere(10, Vector((25, 0, 25)), Material(Vector((0.25, 0.25, 1.0)), 0.5))
-SPHERES[3] = Sphere(10, Vector((50, 0, 25)), Material(Vector((1.0, 1.0, 1.0)), 0.5))
+SPHERES[3] = Sphere(10, Vector((50, 0, 25)), Material(Vector((1.0, 1.0, 1.0)), 1.0))
+SPHERES[4] = Sphere(490, Vector((0, -500, 0)), Material(Vector((1.0, 1.0, 1.0)), 0.0))
 
 
 app = App("Ray Tracing", SIZE, SPHERES)
