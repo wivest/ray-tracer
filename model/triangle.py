b7a8c0b23de087fc28a4791efd70deb748c27998
wivest <120594169+wivest@users.noreply.github.com>
2025-04-15 23:44:38 +0200

    feat: check plane intersection

--- model/triangle.py
+++ model/triangle.py
@@ -14,7 +14,15 @@ class Triangle:
 
     @ti.func
     def intersects(self, ray: Ray) -> f32:  # type: ignore
+        coefficient = -1.0
+
         side1 = self.v2 - self.v1
         side2 = self.v3 - self.v1
         normal = ti.math.cross(side1, side2)
-        return -1.0
+
+        if ti.math.dot(ray.direction, normal) != 0.0:
+            vo = ray.origin - self.v1
+            numerator = ti.math.dot(vo, normal)
+            denominator = ti.math.dot(ray.direction, normal)
+            coefficient = -numerator / denominator
+        return coefficient
