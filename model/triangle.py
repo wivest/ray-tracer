a2136a01ac25977ecd12d664c6b5bdec62001c31
wivest <120594169+wivest@users.noreply.github.com>
2025-04-16 23:17:26 +0200

    feat: implement Moeller-Trumbore algorithm

--- model/triangle.py
+++ model/triangle.py
@@ -8,21 +8,31 @@ from .ray import Ray
 
 @ti.dataclass
 class Triangle:
-    v1: vec3  # type: ignore
-    v2: vec3  # type: ignore
-    v3: vec3  # type: ignore
+    a: vec3  # type: ignore
+    b: vec3  # type: ignore
+    c: vec3  # type: ignore
 
     @ti.func
     def intersects(self, ray: Ray) -> f32:  # type: ignore
-        coefficient = -1.0
+        solution = -1.0
 
-        side1 = self.v2 - self.v1
-        side2 = self.v3 - self.v1
-        normal = ti.math.cross(side1, side2)
+        edgeAB = self.b - self.a
+        edgeAC = self.c - self.a
+        det = self._det(ray.direction, edgeAB, edgeAC)
 
-        if ti.math.dot(ray.direction, normal) != 0.0:
-            vo = ray.origin - self.v1
-            numerator = ti.math.dot(vo, normal)
-            denominator = ti.math.dot(ray.direction, normal)
-            coefficient = -numerator / denominator
-        return coefficient
+        inv_det = 1.0 / det
+        vecAO = ray.origin - self.a
+
+        t = inv_det * self._det(vecAO, edgeAB, edgeAC)
+        u = inv_det * self._det(ray.direction, vecAO, edgeAC)
+        v = inv_det * self._det(ray.direction, edgeAB, vecAO)
+
+        if det != 0.0 and 0.0 < u + v and u + v > 1.0:
+            solution = t
+
+        return solution
+
+    @ti.func
+    def _det(self, col1: vec3, col2: vec3, col3: vec3) -> f32:  # type: ignore
+        cross = ti.math.cross(col2, col3)
+        return ti.math.dot(col1, cross)
