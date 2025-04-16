53f51fd1219f10f722026f8986308f61dcb0931a
wivest <120594169+wivest@users.noreply.github.com>
2025-04-17 00:40:59 +0200

    fix: update ray direction and misses

--- model/triangle.py
+++ model/triangle.py
@@ -4,6 +4,7 @@ from taichi import f32
 from taichi.math import vec3
 
 from .ray import Ray
+from .material import Material
 
 
 @ti.dataclass
@@ -11,6 +12,7 @@ class Triangle:
     a: vec3  # type: ignore
     b: vec3  # type: ignore
     c: vec3  # type: ignore
+    material: Material  # type: ignore
 
     @ti.func
     def intersects(self, ray: Ray) -> f32:  # type: ignore
@@ -18,19 +20,24 @@ class Triangle:
 
         edgeAB = self.b - self.a
         edgeAC = self.c - self.a
-        det = self._det(ray.direction, edgeAB, edgeAC)
+        ray_dir = -ray.direction
+        det = self._det(ray_dir, edgeAB, edgeAC)
 
         inv_det = 1.0 / det
         vecAO = ray.origin - self.a
 
         t = inv_det * self._det(vecAO, edgeAB, edgeAC)
-        u = inv_det * self._det(ray.direction, vecAO, edgeAC)
-        v = inv_det * self._det(ray.direction, edgeAB, vecAO)
+        u = inv_det * self._det(ray_dir, vecAO, edgeAC)
+        v = inv_det * self._det(ray_dir, edgeAB, vecAO)
 
-        if det != 0.0 and 0.0 < u + v and u + v > 1.0:
+        if det != 0.0 and 0.0 < u + v and u + v < 1.0 and u * v > 0.0 and t > 0:
             solution = t
 
         return solution
+
+    @ti.func
+    def normal(self, point: vec3) -> vec3:  # type: ignore
+        return ti.math.cross(self.b - self.a, self.c - self.a)
 
     @ti.func
     def _det(self, col1: vec3, col2: vec3, col3: vec3) -> f32:  # type: ignore
