a394877f77364d405280baef6c69d7e79b40c7d1
wivest <120594169+wivest@users.noreply.github.com>
2025-04-17 00:41:30 +0200

    chore: create triangles scene

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from taichi import Vector
 
 from app.app import App
 from model.sphere import Sphere
+from model.triangle import Triangle
 from model.material import Material
 
 
@@ -16,6 +17,14 @@ SPHERES[2] = Sphere(10, Vector((25, 0, 25)), Material(Vector((0.25, 0.25, 1.0)),
 SPHERES[3] = Sphere(10, Vector((50, 0, 25)), Material(Vector((1.0, 1.0, 1.0)), 1.0))
 SPHERES[4] = Sphere(490, Vector((0, -500, 0)), Material(Vector((1.0, 1.0, 1.0)), 0.0))
 
+TRIANGLES = Triangle.field(shape=1)
+TRIANGLES[0] = Triangle(
+    Vector((0, 0, 0)),
+    Vector((0, 0, 1)),
+    Vector((1, 0, 0)),
+    Material(Vector((1, 1, 1))),
+)
 
-app = App("Ray Tracing", SIZE, SPHERES)
+
+app = App("Ray Tracing", SIZE, TRIANGLES)
 app.run()
