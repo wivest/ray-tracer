cd99c7560bd7ee5ebe0396dbe16291d191e35642
wivest <120594169+wivest@users.noreply.github.com>
2025-04-17 22:20:00 +0200

    chore: create tetrahedron

--- main.py
+++ main.py
@@ -17,11 +17,29 @@ SPHERES[2] = Sphere(10, Vector((25, 0, 25)), Material(Vector((0.25, 0.25, 1.0)),
 SPHERES[3] = Sphere(10, Vector((50, 0, 25)), Material(Vector((1.0, 1.0, 1.0)), 1.0))
 SPHERES[4] = Sphere(490, Vector((0, -500, 0)), Material(Vector((1.0, 1.0, 1.0)), 0.0))
 
-TRIANGLES = Triangle.field(shape=1)
+TRIANGLES = Triangle.field(shape=4)
 TRIANGLES[0] = Triangle(
     Vector((0, 0, 0)),
+    Vector((1, 0, 0)),
+    Vector((0, 0, 1)),
+    Material(Vector((1, 1, 1))),
+)
+TRIANGLES[1] = Triangle(
+    Vector((0, 1, 0)),
     Vector((0, 0, 1)),
     Vector((1, 0, 0)),
+    Material(Vector((1, 1, 1))),
+)
+TRIANGLES[2] = Triangle(
+    Vector((0, 1, 0)),
+    Vector((1, 0, 0)),
+    Vector((0, 0, 0)),
+    Material(Vector((1, 1, 1))),
+)
+TRIANGLES[3] = Triangle(
+    Vector((0, 1, 0)),
+    Vector((0, 0, 0)),
+    Vector((0, 0, 1)),
     Material(Vector((1, 1, 1))),
 )
 
