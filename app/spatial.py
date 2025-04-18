185f00f162aed30a57727d5288c6f80150258dcf
wivest <120594169+wivest@users.noreply.github.com>
2025-04-18 19:08:47 +0200

    feat: parse vertices and faces

--- app/spatial.py
+++ app/spatial.py
@@ -6,13 +6,22 @@ from model.triangle import Triangle
 class Spatial:
 
     def __init__(self, path: str):
-        self.faces = []
+        self.faces: list[
+            tuple[
+                tuple[float, float, float],
+                tuple[float, float, float],
+                tuple[float, float, float],
+            ]
+        ] = []
 
         with open(path) as file:
             self.__parse(file.readlines())
 
     def __parse(self, lines: list[str]):
         vertices: list[tuple[float, float, float]] = []
+
+        def vertex_i(tokens: list[str], i: int) -> int:
+            return int(tokens[i].split("/")[0]) - 1
 
         for line in lines:
             tokens = line.split()
@@ -22,7 +31,10 @@ class Spatial:
                 z = float(tokens[3])
                 vertices.append((x, y, z))
             elif tokens[0] == "f":
-                print(tokens[1], tokens[2], tokens[3])
+                a = vertices[vertex_i(tokens, 1)]
+                b = vertices[vertex_i(tokens, 2)]
+                c = vertices[vertex_i(tokens, 3)]
+                self.faces.append((a, b, c))
 
     def export(self) -> StructField:
         data = Triangle.field()
