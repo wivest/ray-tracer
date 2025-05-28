349b331a20152891067fe28f65b6ff5dbd234d6a
wivest <120594169+wivest@users.noreply.github.com>
2025-05-28 22:35:56 +0200

    feat: export collected spatials

--- model/scene.py
+++ model/scene.py
@@ -1,12 +1,33 @@
 import os
 
+from imports.common import *
+
 from .spatial import Spatial
+from .triangle import Triangle
 
 
+@ti.data_oriented
 class Scene:
     def __init__(self, dir: str):
         self.spatials: list[Spatial] = []
 
         for root, _, files in os.walk(dir):
             for file in files:
+                if not file.endswith(".obj"):
+                    continue
                 self.spatials.append(Spatial(root, file))
+
+    def export(self) -> StructField:
+        n = 0
+        for spatial in self.spatials:
+            n += spatial.triangles.shape[0]
+
+        triangles = Triangle.field(shape=n)
+
+        i = 0
+        for spatial in self.spatials:
+            for t in range(spatial.triangles.shape[0]):
+                triangles[i] = spatial.triangles[t]
+                i += 1
+
+        return triangles
