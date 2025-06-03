6fcade60bd1077d4ddb305c68b7efa37c56166b8
wivest <120594169+wivest@users.noreply.github.com>
2025-06-03 23:12:14 +0200

    feat: parse position

--- model/setup.py
+++ model/setup.py
@@ -1,7 +1,28 @@
+# aliases
+vec = tuple[float, float, float]
+basis = tuple[vec, vec, vec]
+
+
 class Setup:
     @staticmethod
-    def get_camera(path: str):
-        pass
+    def get_camera(path: str) -> tuple[vec, basis]:
+        with open(path) as file:
+            lines = file.readlines()
+
+        camera_pos: vec = (0.0, 0.0, 0.0)
+        camera_basis: basis = ((0.0, 0.0, 0.0), (0.0, 0.0, 0.0), (0.0, 0.0, 0.0))
+
+        for line in lines:
+            tokens = line.split()
+            key = tokens[0]
+
+            if key == "cp":
+                x = float(tokens[1])
+                y = float(tokens[2])
+                z = float(tokens[3])
+                camera_pos = (x, y, z)
+
+        return camera_pos, camera_basis
 
     @staticmethod
     def get_light(path: str):
