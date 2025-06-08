407ce840ed39fde59b91dd90d8fe5b7978af30f4
wivest <120594169+wivest@users.noreply.github.com>
2025-06-08 23:51:02 +0200

    feat: create __init__.py

---
+++ model/__init__.py
@@ -0,0 +1,10 @@
+from .gltf_parser import *
+from .material import *
+from .setup import *
+from .spatial import *
+from .sphere import *
+from .triangle import *
+
+# aliases
+vec = tuple[float]
+basis = tuple[vec, vec, vec]
