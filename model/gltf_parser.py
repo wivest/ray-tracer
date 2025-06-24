5bdf454c71e800f3f5792c25fc3422b5fbd52a46
wivest <120594169+wivest@users.noreply.github.com>
2025-06-24 23:58:05 +0200

    refactor: remove triangles and indices

--- model/gltf_parser.py
+++ model/gltf_parser.py
@@ -1,7 +1,5 @@
-from imports.aliases import vec, basis
-
 import struct
-from pygltflib import GLTF2, Accessor, BufferView
+from pygltflib import GLTF2
 from scipy.spatial.transform import Rotation
 
 from camera.transform import Transform
@@ -41,49 +39,3 @@ def __convert_transform(translation: list[float], rotation: list[float]) -> Tran
     bas = tuple(tuple(i) for i in mat.tolist())
 
     return Transform(origin, bas)  # type: ignore
-
-
-def get_triangles(path: str):
-    gltf = GLTF2().load(path)
-    if gltf == None:
-        raise Exception()
-
-    primitives = gltf.meshes[0].primitives
-
-    accessor: Accessor = gltf.accessors[primitives[0].attributes.POSITION]  # type: ignore
-    bufferView: BufferView = gltf.bufferViews[accessor.bufferView]  # type: ignore
-    buffer = gltf.buffers[bufferView.buffer]
-    data = gltf.get_data_from_buffer_uri(buffer.uri)
-
-    if type(data) is not bytes:
-        raise Exception()
-    if bufferView.byteOffset == None:
-        raise Exception()
-    TYPE_SIZE = 12  # accessor.type = "VEC3"
-    for i in range(accessor.count):
-        idx = bufferView.byteOffset + i * TYPE_SIZE
-        a, b, c = struct.unpack("fff", data[idx : idx + 12])
-        print(a, b, c)
-
-
-def get_indices(path: str):
-    gltf = GLTF2().load(path)
-    if gltf == None:
-        raise Exception()
-
-    primitives = gltf.meshes[0].primitives
-
-    accessor: Accessor = gltf.accessors[primitives[0].indices]  # type: ignore
-    bufferView: BufferView = gltf.bufferViews[accessor.bufferView]  # type: ignore
-    buffer = gltf.buffers[bufferView.buffer]
-    data = gltf.get_data_from_buffer_uri(buffer.uri)
-
-    if type(data) is not bytes:
-        raise Exception()
-    if bufferView.byteOffset == None:
-        raise Exception()
-    TYPE_SIZE = 2  # accessor.type = "SCALAR"
-    for i in range(accessor.count // 3):
-        idx = bufferView.byteOffset + i * TYPE_SIZE * 3
-        ns = struct.unpack("HHH", data[idx : idx + TYPE_SIZE * 3])
-        print(ns)
