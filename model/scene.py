7b78e3403c69c7f64d3b55cf627b7a868f41662a
wivest <120594169+wivest@users.noreply.github.com>
2025-07-03 23:43:05 +0200

    feat: create scene gltf loader

---
+++ model/scene.py
@@ -0,0 +1,23 @@
+from pygltflib import GLTF2
+
+from imports.common import *
+
+from .spatial import Spatial
+
+
+@ti.data_oriented
+class Scene:
+
+    def __init__(self, path: str):
+        gltf = GLTF2().load(path)
+        if gltf == None:
+            raise Exception()
+
+        self.spatials: list[Spatial] = []
+        nodes = gltf.scenes[gltf.scene].nodes or []
+        for i in nodes:
+            node = gltf.nodes[i]
+            if node.mesh == None:
+                continue
+            mesh = gltf.meshes[node.mesh]
+            self.spatials.append(Spatial(mesh, gltf))
