f944fc25cb0168dec83da99a97ee96b8d5013c24
wivest <120594169+wivest@users.noreply.github.com>
2025-07-05 23:27:17 +0200

    feat: create Scene in main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import taichi as ti
 from pygltflib import GLTF2
 
 from app.app import App
-from model.spatial import Spatial
+from model.scene import Scene
 
 
 ti.init(arch=ti.gpu)
@@ -13,10 +13,7 @@ DEFAULT_SCENE = "./scene/"
 DEFAULT_GLTF = DEFAULT_SCENE + "untitled.gltf"
 
 gltf_path = sys.argv[1] if len(sys.argv) == 2 else DEFAULT_GLTF
-gltf = GLTF2().load(gltf_path)
-if gltf == None:
-    raise Exception()
-spatial = Spatial(gltf.meshes[0], gltf)
+spatial = Scene(gltf_path)
 
 app = App("Ray Tracing", SIZE, spatial.export(), DEFAULT_GLTF)
 app.run()
