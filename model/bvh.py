070432629d22d91c6a93fbaaf2c4cac92d65a28a
wivest <120594169+wivest@users.noreply.github.com>
2025-07-22 23:58:04 +0200

    feat: create AABB with bounding box

--- model/bvh.py
+++ model/bvh.py
@@ -1,5 +1,14 @@
 from imports.common import *
 
+from .bounding_box import BoundingBox
+
+
+@ti.dataclass
+class AABBwTriangles:
+    aabb: BoundingBox  # type: ignore
+    start: int
+    length: int
+
 
 @ti.dataclass
 class BVH:
