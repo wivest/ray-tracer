43651ac6ae0bfeb20a90a8b2400828e0e5d5f9b9
wivest <120594169+wivest@users.noreply.github.com>
2025-09-11 14:06:22 +0200

    feat: parse render and size options

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ from app.app import App
 from model.scene import Scene
 
 
-ti.init(arch=ti.gpu)
-
 SIZE = (1080, 720)
 DEFAULT_GLTF = "./scene/untitled.gltf"
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--render", action="store_true")
+parser.add_argument("-x", "--width", default=SIZE[0], type=int)
+parser.add_argument("-y", "--height", default=SIZE[1], type=int)
 parser.add_argument("filename", nargs="?", default=DEFAULT_GLTF)
 args = parser.parse_args()
 
 
-scene = Scene(args.filename)
+ti.init(arch=ti.gpu)
 
+scene = Scene(args.filename)
 triangles, bvhs = scene.export()
 
-app = App("Ray Tracing", SIZE, triangles, bvhs, DEFAULT_GLTF)
-app.run()
+app = App("Ray Tracing", (args.width, args.height), triangles, bvhs, DEFAULT_GLTF)
+if args.render:
+    app.run_render()
+else:
+    app.run()
